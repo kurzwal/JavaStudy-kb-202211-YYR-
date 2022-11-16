01cd636cf237500714b86f8b195d23c288b64235
kurzwal <openingyou@gmail.com>
2022-11-16 13:07:16 +0900

    챕터 01 출력

---
+++ src/main/java/j01_출력/SystemOut.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class SystemOut {
+	public static void main(String[] args) {
+		System.out.print("이름 : ");
+		System.out.println("유열림");
+		System.out.print("주소 : ");
+		System.out.println("부산 사상구 사상로");
+		System.out.print("연락처 : ");
+		System.out.println("010-3112-3112");
+		System.out.print("성별 : ");
+		System.out.println("남성");
+	}
+}
---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,15 @@
+package j01_출력;
+
+public class UserInfo {
+	public static void main(String[] args) {
+		
+		String name = "유열림";
+		String address = "부산 사상구 사상로";
+		String phone = "010-3112-3112";
+		int age = 29;
+		
+		System.out.println(name + "의 주소는 " + address + "입니다.");
+		System.out.println(name + "의 연락처는 " + phone + "입니다.");
+		System.out.println(name + "의 나이는 " + age + "입니다.");
+	}
+}
