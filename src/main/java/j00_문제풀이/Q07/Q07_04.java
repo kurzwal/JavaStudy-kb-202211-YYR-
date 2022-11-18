014c21f0e8189b704e1f88690a7907d58eb974e7
kurzwal <openingyou@gmail.com>
2022-11-18 13:07:20 +0900

    문제풀이 7단원

---
+++ src/main/java/j00_문제풀이/Q07/Q07_04.java
@@ -0,0 +1,20 @@
+//package j00_문제풀이.Q07;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//public class Q07_04 {
+//	public static void main(String[] args) {
+//		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//		int A = sc.nextInt();
+//		int B = sc.nextInt();
+//		int V = sc.nextInt();
+//		int S = A-B;
+//		int result = 1;
+//		result += (V - A)/S;
+//		if ((V - A)%S != 0) {
+//			result += 1;
+//		}
+//		System.out.println(result);
+//	}
+//}
