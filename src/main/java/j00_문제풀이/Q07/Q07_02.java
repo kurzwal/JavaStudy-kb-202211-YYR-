da7f91b82690a22e9a22f7db9e4be2c2d837dd8c
kurzwal <openingyou@gmail.com>
2022-12-09 12:54:40 +0900

    변경사항 없음

--- src/main/java/j00_문제풀이/Q07/Q07_02.java
+++ src/main/java/j00_문제풀이/Q07/Q07_02.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Q07_02 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int bee = sc.nextInt();
-		bee += 4;
-		bee /= 6;
-		double result = (Math.sqrt(1 + 8. * bee) - 1) / 2;
-		System.out.println((int) Math.ceil(result)+1);
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			int bee = sc.nextInt();
+			bee += 4;
+			bee /= 6;
+			double result = (Math.sqrt(1 + 8. * bee) - 1) / 2;
+			System.out.println((int) Math.ceil(result)+1);
+		}
 	}
 }
